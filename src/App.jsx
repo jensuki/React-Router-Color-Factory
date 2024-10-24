a72d03448bce047be647aa381a45490a1dad1e73
jensuki <jeni.suk@proton.me>
2024-10-23 18:29:35 -0700

    added local storage

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,22 @@ import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import ColorList from './ColorList';
 import ColorDetail from './ColorDetail';
 import NewColorForm from './NewColorForm';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import './App.css'
 
 function App() {
-  const [colors, setColors] = useState([
+  // use local storaged colors or default colors
+  const initialColors = JSON.parse(localStorage.getItem('colors')) || [
     { name: 'red', value: '#FF0000' },
     { name: 'green', value: '#00FF00' },
-    { name: 'blue', value: '#0000FF' }
-  ]);
+    { name: 'blue', value: '#0000FF' }];
+
+  const [colors, setColors] = useState(initialColors);
+
+  // save colors to local storage whenever the color changes
+  useEffect(() => {
+    localStorage.setItem('colors', JSON.stringify(colors))
+  }, [colors])
 
   const addColor = (newColor) => {
     setColors([...colors, newColor]); // update state when adding new colors
